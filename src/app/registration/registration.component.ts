bc3e7c5778df23e6de2850a61aa3922c34b45d2e
mohd229 <mohdammarahmedmasood@gmail.com>
2024-10-07 13:35:34 +0530

    .

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void{
+    this.firstFormGroup = this.fb.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this.fb.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+  
+  isLinear = false;
 
 }
